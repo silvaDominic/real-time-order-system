2631d0e782adfb73908b1c3c69cc74214e7565d8
Dominic <string.reverse@proton.me>
2024-09-05 00:41:58 -0400

    Adjusted filter function to maintain filter as new orders come in

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ReactHTMLElement, useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 
 import { SocketService } from "../../core/socket.service";
@@ -8,13 +8,22 @@ import { OrderTable } from "../components/order/order-table.component";
 
 function App() {
   const [orders, setOrders] = useState<OrderViewModel[]>([]);
+  const [filteredOrders, setFilteredOrders] = useState<OrderViewModel[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
 
   useEffect(() => {
     SocketService.connect();
 
     SocketService.on('order_event', (data: any) => {
-      setOrders([...orders, ...data.map((item: any) => mapOrder(item))]);
+      const incomingOrders = data.map((item: any) => ({[item.id]: item})); // Mapping by id gives O(1) look up times
+      setOrders(data.map((item: any) => {
+        // If order already exists, update it
+        if (incomingOrders[item.id] !== undefined) {
+          return mapOrder(incomingOrders[item]);
+        }
+        // Otherwise return item
+        return mapOrder(item);
+      }));
     });
 
     return () => {
@@ -25,15 +34,18 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    setFilteredOrders(orders.filter((item: OrderViewModel) => item.price.toString().includes(searchQuery)));
+  }, [searchQuery, orders]);
+
   function onSearch(event: ChangeEvent<HTMLInputElement>): void {
     setSearchQuery(event.target.value);
-    setOrders([...orders].filter((item: OrderViewModel) => item.price.toString().includes(searchQuery)));
   }
 
   return (
     <div className="App">
       <input id='search-field' placeholder="Search by price..." type="text" value={searchQuery} onChange={onSearch}/>
-      <OrderTable orders={orders} />
+      <OrderTable orders={filteredOrders} />
     </div>
   );
 }
